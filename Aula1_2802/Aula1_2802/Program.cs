f31cc03eb8db7b3f75bcaf35fdb6483bd6db3621
FELIPE <FELIPE@FELIPE-PC>
2018-02-28 23:26:24 -0300

    escreve e lê

---
+++ Aula1_2802/Aula1_2802/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_2802
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+
+
+            Console.WriteLine("Digite Seu Nome");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine("Olá " + p.Nome);
+            Console.ReadKey();
+        }
+    }
+}
