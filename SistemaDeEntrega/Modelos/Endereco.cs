dacfa88030e780d9f6d0207899d4d1c4cd48d24b
FELIPE <FELIPE@FELIPE-PC>
2018-02-28 23:49:30 -0300

    Projeto

---
+++ SistemaDeEntrega/Modelos/Endereco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+     public class Endereco 
+    {
+        public int EnderecoID { get; set; }
+
+        public string Rua { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+    }
+}
---
+++ SistemaDeEntrega/Modelos/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+  public abstract class Pessoa
+    {
+        public int PessoaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public Endereco _Endereco { get; set; }
+
+        public int EnderecoID { get; set; }
+    }
+}
