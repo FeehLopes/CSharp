93339dd2a7fcf66af12a81a7bd92df69c2a42fa4
FELIPE <FELIPE@FELIPE-PC>
2018-03-07 20:22:07 -0300

    Adicionando metodo SalvarCliente

---
+++ SistemaDeEntrega/Controller/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class ClienteController
+    {
+        public void SalvarCliente(Cliente cliente)
+        {
+            //TODO: Percistir os dados do Cliente
+        }
+    }
+}
