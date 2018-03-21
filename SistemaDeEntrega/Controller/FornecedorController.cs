bd1e2cca793373f5a2cb5cefc7902ab28aa36b5c
FELIPE <FELIPE@FELIPE-PC>
2018-03-20 23:08:08 -0300

    Fornecedor e Endereço Controller

---
+++ SistemaDeEntrega/Controller/FornecedorController.cs
@@ -0,0 +1,23 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+   public  class FornecedorController
+    {
+        static List<Fornecedor> FornecedoresCadastrados = new List<Fornecedor>();
+        static int ultimaID = 0;
+
+       
+        public void SalvarFornecedor(Fornecedor fornecedor)
+        {
+            fornecedor.PessoaID = ultimaID + 1;
+            FornecedoresCadastrados.Add(fornecedor);
+            ultimaID++;
+        }
+    }
+}
