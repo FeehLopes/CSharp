d4124dc51917770f47642490ffab6789f0384f9a
FELIPE <FELIPE@FELIPE-PC>
2018-03-28 22:02:09 -0300

    WPF LAYOUT

---
+++ SistemaDeEntrega/WpfViewsw/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfViewsw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroCliente cadCli = new CadastroCliente();
+
+            //cadCli.Show();
+            cadCli.ShowDialog();
+        }
+
+        private void btnEntregador_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroEntregador cadEntregador = new CadastroEntregador();
+
+            cadEntregador.ShowDialog();
+        }
+
+        private void btnFornecedor_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroFornecedor cadFornecedor = new CadastroFornecedor();
+
+            cadFornecedor.ShowDialog();
+        }
+    }
+}
